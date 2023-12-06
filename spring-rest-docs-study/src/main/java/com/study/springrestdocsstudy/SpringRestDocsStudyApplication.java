6a08cd5c98590b1d24e966ab9a66bc39b95cdea2
laon198 <laon198@gmail.com>
2023-12-06 20:43:33 +0900

    spring-rest-docs init

---
+++ spring-rest-docs-study/src/main/java/com/study/springrestdocsstudy/SpringRestDocsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.springrestdocsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDocsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestDocsStudyApplication.class, args);
+    }
+
+}
---
+++ spring-rest-docs-study/src/test/java/com/study/springrestdocsstudy/SpringRestDocsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springrestdocsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestDocsStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
